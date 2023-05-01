900bcd218e60951e346f551b27ba4d0bafab89f8
Cesar <cesar_a_mar_@hotmail.com>
2023-05-01 10:52:51 -0500

    Creacion del proyecto

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] float speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRb.velocity.y);
+    }
+}
